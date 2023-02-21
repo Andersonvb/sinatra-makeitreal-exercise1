22a08e991cce9ddd311e144d17f78b0e7b2ae1fd
Andersonvb <andersonvalenciabermudez@gmail.com>
2023-02-21 17:23:30 -0500

    fix bug

--- solution.rb
+++ solution.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 get '/' do
   if params[:nombre]
-    params[:nombre].empty? ? 'desconocido' : @nombre = params[:nombre]
+    @nombre = params[:nombre].empty? ? 'desconocido' : params[:nombre]
   else
     @nombre = 'desconocido'
   end
